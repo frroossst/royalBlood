702d17977152d33cda63d2e7dd219ff8107e2b03
frroossst <adhyanpatel@gmail.com>
2021-08-11 19:00:31 +0530

    0.0.2 - creating a node & adding children

--- tree.py
+++ tree.py
@@ -1,15 +1,39 @@
-# dict fmt => {"parent" : nodes}
-# 
-# 
+# dict fmt => {"parent" : [children, ]}
+
+import json
+
+treeGraph = {}
 
 class Node():
 
     def __init__(self) -> None:
         pass
 
-    def addChild(self,name):
+    def addNode(self,name):
         self.name = name
-        print(f"{name} added to tree")
+        if treeGraph == {}:
+            treeGraph[self.name] = []
+            
+        for i in treeGraph:           
+            if i == self.name:
+                pass
+            else:
+                treeGraph[self.name] = []
+                break
+        return treeGraph
+
+    def addChildren(self,parent,children): #children is a list of strings
+        self.parent = parent
+        self.children = children
+        for i in treeGraph:
+            if i == self.parent:
+                treeGraph[self.parent] = self.children
+            else:
+                raise KeyError ("Parent node does not exist")
+
+
 
 N = Node()
-N.addChild("adhyan")
+N.addNode("Elizabeth")
+N.addChildren("Elizabeth",["Charles", "Andrew"])
+print(treeGraph)
