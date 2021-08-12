13d75abe0187aae58b117cdc84539927173beef9
frroossst <adhyanpatel@gmail.com>
2021-08-12 16:15:46 +0530

    0.0.4 - using separate dicts for attributes and tree

--- tree.py
+++ tree.py
@@ -1,8 +1,10 @@
-# dict fmt => {"parent" : [children, ]}
+# attribute dict fmt => {"parent" : [{"children" : []}, {"name" : "NAME"} ], {...}}
+# tree dict fmt => {"parent" : [child0,child1], "child0" : None, "child1" : None, ...}
 
 import json
 
 treeGraph = {}
+attributeDict = {}
 
 class Node():
 
@@ -10,36 +12,28 @@ class Node():
         pass
 
     def __str__(self):
-        for i in treeGraph:
-            print(i)
-            print(treeGraph[i])
-            print("_________")
+        print(treeGraph)
 
     def addNode(self,name):
         self.name = name
-        if treeGraph == {}:
+        if self.name not in treeGraph:
             treeGraph[self.name] = []
-            
-        for i in treeGraph:           
-            if i == self.name:
-                pass
-            else:
-                treeGraph[self.name] = []
-                break
-        return treeGraph
+        else:
+            raise KeyError ("Node already exists")
+
 
     def addChildren(self,parent,children): #children is a list of strings
         self.parent = parent
         self.children = children
-        for i in treeGraph:
-            if i == self.parent:
-                treeGraph[self.parent] = self.children
-            else:
-                pass
+        if self.parent in treeGraph:
+            treeGraph[self.parent] = self.children
+        else:
+            raise KeyError ("Node does not exist")
 
 
 
 N = Node()
+
 N.addNode("Elizabeth")
 N.addNode("Mary")
 
