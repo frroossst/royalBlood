91f594e7a988b160f932ade313f8727dffc5e3bb
frroossst <adhyanpatel@gmail.com>
2021-08-13 21:44:55 +0530

    0.0.6 - work on printing n ary BFS algo

--- tree.py
+++ tree.py
@@ -22,31 +22,40 @@ class Node():
         else:
             raise KeyError ("Node already exists")
 
-    def updateNode(self,node):
+    def updateNode(self,node,what):
         self.node = node
+        self.what = what
+
         if self.node not in treeGraph:
             raise KeyError ("Node does not exist")
         else:
-            title = input("enter title : ")
-            attributeDict[self.node]["title"] = title
-            house = input("enter house name : ")
-            attributeDict[self.node]["house"] = house
-            age = int(input("enter age : "))
-            attributeDict[self.node]["age"] = age
-            marriage = input("married to : ")
-            attributeDict[self.node]["married to"] = marriage
-            children = []
-            print("enter list of children : ")
-            while True:
-                addChild = input("enter child name : ")
-                if addChild != "/break":
-                    children.append(addChild)
-                else:
-                    break
-            Node.addChildren(self,self.node,children)
-            attributeDict[self.node]["children"] = children
-            for i in children:
-                treeGraph[i] = None
+            if self.what == "title":
+                title = input("enter title : ")
+                attributeDict[self.node]["title"] = title
+            elif self.what == "house":
+                house = input("enter house name : ")
+                attributeDict[self.node]["house"] = house
+            elif self.what == "age":
+                age = int(input("enter age : "))
+                attributeDict[self.node]["age"] = age
+            elif self.what == "marriage":
+                marriage = input("married to : ")
+                attributeDict[self.node]["married to"] = marriage
+            elif self.what == "children":
+                children = []
+                print("enter list of children : ")
+                while True:
+                    addChild = input("enter child name : ")
+                    if addChild != "/break":
+                        children.append(addChild)
+                    else:
+                        break
+                Node.addChildren(self,self.node,children)
+                attributeDict[self.node]["children"] = children
+                for i in children:
+                    treeGraph[i] = None
+            else:
+                raise SyntaxError ("what argument not passed")
 
     def addChildren(self,parent,children): #children is a list of strings
         self.parent = parent
@@ -56,16 +65,20 @@ class Node():
         else:
             raise KeyError ("Node does not exist")
 
-
+    @classmethod
+    def printTree(self):
+        visited = []
+        for i in treeGraph:
+            if i not in visited:
+                visited.append(i)
+                print(i)
 
 N = Node()
 
 N.addNode("Elizabeth")
 N.addNode("Mary")
-N.updateNode("Elizabeth")
-
-# N.addChildren("Elizabeth",["Charles", "Andrew"])
-# N.addChildren("Mary",["Francois","Charlie"])
-
+N.updateNode("Elizabeth","children")
+print(type(treeGraph["Lise"]))
 print(treeGraph)
 print(attributeDict)
+N.printTree()
